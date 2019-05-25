25a2bcf70236eb1491aedc4a20705e7ca78040a7
root <juandiemore@gmail.com>
2019-05-25 17:07:15 -0500

    agregando actualizar y borrar

--- src/routes/book.js
+++ src/routes/book.js
@@ -10,23 +10,57 @@ routes.get('/all', (req,res,next)=>{
 });
 
 routes.get('/:id',(req,res,next)=> {
-  db.books.findOne({_id: req.params.id},(err,books)=>{
+  db.books.findOne({_id: mongojs.ObjectId(req.params.id)},(err,books)=>{
     if(err) return next(err);
     res.json(books);
   });
 });
 
+// add
 routes.post('/add',(req,res,next)=>{
   const book = req.body;
+  // condicional
   if(!book.title){
     res.status(400).json({
       err: 'Bad data'
     });
-  } else {
-    db.book.save(book, (err, task) =>{
-      res.json(task);
+// add the new book
+  }else {
+    db.book.save(book, (err, book) =>{
+      if(err) return next(err);
+      res.json(book);
     });
   }
+});
+
+routes.delete('/delete/:id', (req,res,next)=>{
+  db.books.remove({_id: mongojs.ObjectId(req.params.id)}, (err,result)=>{
+    if(err) return next(err);
+    res.json(result);
+      });
+  });
+
+
+router.put('/actualizar/:id', (req,res,next)=>{
+  const book = req.body;
+  const updateBook = {};
+  if(book.isDone) {
+    updateBook.isDone = book.isDone;
+  }
+  if(book.title){
+    updateBook.title = book.title;
+  }
+
+  if(!updateBook){
+  res.status(400).json({
+    error: 'Bad request'
+        });
+    }else{
+  db.books.update({_id: mongojs.ObjectId(req.paramas.id)}, (err, book)=>{
+    if(err) return next(err);
+    res.json(book);
+      });
+    }
 });
 
 module.exports = routes;
